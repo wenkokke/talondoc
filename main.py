0f4c2bfac61b44245b94a7b0bf65cea9555da3c3
Wen Kokke <wenkokke@users.noreply.github.com>
2022-06-30 16:07:14 +0100

    Change main

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from george.analysis.info import *
 from george.analysis.python import *
 
 for python_file in Path("vendor").glob("**/*.py"):
-  python_file_info_1 = PythonInfoVisitor(python_file).process()
-  python_file_info_2 = PythonInfo.from_json(python_file_info_1.to_json())
+    python_file_info_1 = PythonInfoVisitor(python_file).process()
+    print(python_file_info_1)
