1915b9b25d3f649b33b98764b408d41514c904e5
Wen Kokke <wenkokke@users.noreply.github.com>
2022-07-05 15:16:53 +0100

    Create JSON files

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ from george.tree_sitter.node_types import *
 from george.tree_sitter.talon import TreeSitterTalon
 from george.tree_sitter.type_provider import *
 
+build_dir = Path("build")
+build_dir.mkdir(exist_ok=True)
+
+knausj_talon_build_dir = build_dir / "knausj_talon"
+knausj_talon_build_dir.mkdir(exist_ok=True)
 
 package_root = Path("vendor/knausj_talon")
 python_analyser = PythonAnalyser()
 python_package_info = python_analyser.process_package(package_root)
-# print(python_package_info.to_json())
-# Path("knausj_talon.py_info.json").write_text(json)
+(knausj_talon_build_dir / "python_package_info.json").write_text(python_package_info.to_json())
 
 tree_sitter_talon = TreeSitterTalon(repository_path="vendor/tree-sitter-talon")
 talon_analyser = TalonAnalyser(python_package_info, tree_sitter_talon)
 talon_package_info = talon_analyser.process_package(package_root)
-print(talon_package_info.to_json())
-# Path("knausj_talon.talon_info.json").write_text(json)
+(knausj_talon_build_dir / "talon_package_info.json").write_text(talon_package_info.to_json())
