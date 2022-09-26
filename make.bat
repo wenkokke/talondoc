0a40468a1f0c05f451c33cf433365a5dfad615c6
Wen Kokke <wenkokke@users.noreply.github.com>
2022-09-26 12:20:54 +0100

    No start

--- make.bat
+++ make.bat
@@ -1,10 +1,10 @@
 @echo off
 
 echo "Install TalonDoc using Talon's Python"
-start "pip" %AppData%\talon\.venv\Scripts\python.exe -m pip install .[docs]
+%AppData%\talon\.venv\Scripts\python.exe -m pip install .[docs]
 
 echo "Generate documentation"
-start "talondoc" %AppData%\talon\.venv\Scripts\talondoc.exe autogen --output-dir example/docs/knausj_talon --sphinx-root example/docs --project 'knausj_talon' --package-name user --no-generate-conf --exclude '*.py' --exclude 'modes/wake_up_wav2letter.talon' example/knausj_talon/
+%AppData%\talon\.venv\Scripts\talondoc.exe autogen --output-dir example/docs/knausj_talon --sphinx-root example/docs --project 'knausj_talon' --package-name user --no-generate-conf --exclude '*.py' --exclude 'modes/wake_up_wav2letter.talon' example/knausj_talon/
 
 echo "Build documentation"
-start "sphinx-build" %AppData%\talon\.venv\Scripts\sphinx-build.exe -M "html" "example/docs" "example/docs/_build"
+%AppData%\talon\.venv\Scripts\sphinx-build.exe -M "html" "example/docs" "example/docs/_build"
