115c92b8e6e0f73f7b0827e2b50ce94fd8afdb67
Wen Kokke <wenkokke@users.noreply.github.com>
2022-09-26 13:24:36 +0100

    Try to use venv?

--- make.bat
+++ make.bat
@@ -1,13 +1,24 @@
 @echo off
 
-echo "Install TalonDoc using Talon's Python"
-%AppData%\talon\.venv\Scripts\python.exe -m pip install --disable-pip-version-check .[docs]
+IF NOT EXIST ".\env" (
+  echo "Install virtualenv"
+  %AppData%\talon\.venv\Scripts\python.exe -m pip install --user virtualenv
+
+  echo "Create virtualenv for TalonDoc"
+  %AppData%\talon\.venv\Scripts\python.exe -m venv env
+)
+
+echo "Activate virtualenv"
+.\env\Scripts\activate
+
+echo "Install TalonDoc"
+...\env\Scripts\python.exe -m pip install .[docs]
 
 echo "Generate documentation"
-%AppData%\talon\.venv\Scripts\talondoc.exe autogen --output-dir "example/docs/knausj_talon" --sphinx-root "example/docs" --project "knausj_talon" --package-name "user" --no-generate-conf --exclude "tests/**" --exclude "*.py" --exclude "modes/wake_up_wav2letter.talon" "example/knausj_talon/"
+...\env\Scripts\talondoc.exe autogen --output-dir "example/docs/knausj_talon" --sphinx-root "example/docs" --project "knausj_talon" --package-name "user" --no-generate-conf --exclude "tests/**" --exclude "*.py" --exclude "modes/wake_up_wav2letter.talon" "example/knausj_talon/"
 
 echo "Clean up cache"
-%AppData%\talon\.venv\Scripts\sphinx-build.exe -M "clean" "example/docs" "example/docs/_build"
+...\env\Scripts\sphinx-build.exe -M "clean" "example/docs" "example/docs/_build"
 
 echo "Build documentation"
-%AppData%\talon\.venv\Scripts\sphinx-build.exe -M "html" "example/docs" "example/docs/_build"
+...\env\Scripts\sphinx-build.exe -M "html" "example/docs" "example/docs/_build"
