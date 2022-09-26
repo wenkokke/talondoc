46f364c4870f2bdf3700318e93e62fe152b9b733
Wen Kokke <wenkokke@users.noreply.github.com>
2022-09-26 12:15:45 +0100

    Change editorconfig for bat files.

--- make.bat
+++ make.bat
@@ -1,5 +1,8 @@
+echo "Install TalonDoc using Talon's Python"
 %AppData%\talon\Scripts\python.exe -m pip install .[docs]
 
+echo "Generate documentation"
 %AppData%\talon\Scripts\talondoc.exe autogen --output-dir example/docs/knausj_talon --sphinx-root example/docs --project 'knausj_talon' --package-name user --no-generate-conf --exclude '*.py' --exclude 'modes/wake_up_wav2letter.talon' example/knausj_talon/
 
+echo "Build documentation"
 %AppData%\talon\Scripts\sphinx-build.exe -M "html" "example/docs" "example/docs/_build"
