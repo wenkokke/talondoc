560c76598e41bcc825147601868f8dbf09f71b90
Wen Kokke <wenkokke@users.noreply.github.com>
2022-09-26 13:06:10 +0100

    Try new batch

--- make.bat
+++ make.bat
@@ -1,10 +1,13 @@
 @echo off
 
 echo "Install TalonDoc using Talon's Python"
-%AppData%\talon\.venv\Scripts\python.exe -m pip install .[docs]
+%AppData%\talon\.venv\Scripts\python.exe -m pip install --disable-pip-version-check .[docs]
 
 echo "Generate documentation"
-%AppData%\talon\.venv\Scripts\talondoc.exe autogen --output-dir example/docs/knausj_talon --sphinx-root example/docs --project 'knausj_talon' --package-name user --no-generate-conf --exclude '*.py' --exclude 'modes/wake_up_wav2letter.talon' example/knausj_talon/
+%AppData%\talon\.venv\Scripts\talondoc.exe autogen --output-dir "example/docs/knausj_talon" --sphinx-root "example/docs" --project "knausj_talon" --package-name "user" --no-generate-conf --exclude "tests/**" --exclude "*.py" --exclude "modes/wake_up_wav2letter.talon" "example/knausj_talon/"
+
+echo "Clean up cache"
+%AppData%\talon\.venv\Scripts\sphinx-build.exe -M "clean" "example/docs" "example/docs/_build"
 
 echo "Build documentation"
 %AppData%\talon\.venv\Scripts\sphinx-build.exe -M "html" "example/docs" "example/docs/_build"
