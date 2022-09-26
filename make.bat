8416bfd6b3c729e627e8f3578aa8552dda9262c8
Wen Kokke <wenkokke@users.noreply.github.com>
2022-09-26 12:19:47 +0100

    Add .venv

--- make.bat
+++ make.bat
@@ -1,10 +1,10 @@
 @echo off
 
 echo "Install TalonDoc using Talon's Python"
-start "pip" %AppData%\talon\Scripts\python.exe -m pip install .[docs]
+start "pip" %AppData%\talon\.venv\Scripts\python.exe -m pip install .[docs]
 
 echo "Generate documentation"
-start "talondoc" %AppData%\talon\Scripts\talondoc.exe autogen --output-dir example/docs/knausj_talon --sphinx-root example/docs --project 'knausj_talon' --package-name user --no-generate-conf --exclude '*.py' --exclude 'modes/wake_up_wav2letter.talon' example/knausj_talon/
+start "talondoc" %AppData%\talon\.venv\Scripts\talondoc.exe autogen --output-dir example/docs/knausj_talon --sphinx-root example/docs --project 'knausj_talon' --package-name user --no-generate-conf --exclude '*.py' --exclude 'modes/wake_up_wav2letter.talon' example/knausj_talon/
 
 echo "Build documentation"
-start "sphinx-build" %AppData%\talon\Scripts\sphinx-build.exe -M "html" "example/docs" "example/docs/_build"
+start "sphinx-build" %AppData%\talon\.venv\Scripts\sphinx-build.exe -M "html" "example/docs" "example/docs/_build"
