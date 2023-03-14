1e68449ed134de9c47e8bf06cf873ea0ff62164c
Wen Kokke <wenkokke@users.noreply.github.com>
2023-03-14 18:18:45 +0000

    Propagate error in build.sh

--- example/build.sh
+++ example/build.sh
@@ -1,19 +1,42 @@
 #!/bin/sh
 
+# POSIX compliant method for 'pipefail':
+fail=$(mktemp)
+
 script_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 output_dir="$script_dir/docs"
 
-talondoc autogen \
-		--output-dir "$output_dir/knausj_talon" \
-		--sphinx-root "$output_dir" \
-		--project 'knausj_talon' \
-		--package-name user \
-		--no-generate-conf \
-		--exclude 'conftest.py' \
-		--exclude 'test/**' \
-		--exclude '**/*.py' \
-		"$script_dir/knausj_talon/"
+[ ! -s "$fail" ] && \
+  talondoc autogen \
+  --output-dir "$output_dir/knausj_talon" \
+  --sphinx-root "$output_dir" \
+  --project 'knausj_talon' \
+  --package-name user \
+  --no-generate-conf \
+  --exclude 'conftest.py' \
+  --exclude 'test/**' \
+  "$script_dir/knausj_talon/" \
+  || echo > "$fail"
 
-sphinx-build -M "clean" "$output_dir" "$output_dir/_build"
+[ ! -s "$fail" ] && \
+  sphinx-build \
+  -M "clean" \
+  "$output_dir" \
+  "$output_dir/_build" \
+  || echo > "$fail"
 
-sphinx-build -M "html" "$output_dir" "$output_dir/_build"
+[ ! -s "$fail" ] && \
+  sphinx-build \
+  -M "html" \
+  "$output_dir" \
+  "$output_dir/_build" \
+  || echo > "$fail"
+
+if [ -s "$fail" ]; then
+    rm "$fail"
+    echo "Could not build example documentation for knausj_talon" >&2
+    exit 1
+else
+    rm "$fail"
+    exit 0
+fi
