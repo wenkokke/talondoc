2ce2a87b65777fa4024f1632b41586dbb92660a4
Wen Kokke <wenkokke@users.noreply.github.com>
2023-03-20 01:32:12 +0000

    Pass types to Sphinx

--- talondoc/sphinx/__init__.py
+++ talondoc/sphinx/__init__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from typing import Any, Optional, Sequence, Union
 
 from sphinx.application import Sphinx
-from typing_extensions import TypedDict
+from typing_extensions import TypeAlias, TypedDict
 
 from .. import __version__
 from .domains import TalonDomain
@@ -20,15 +20,28 @@ TalonPackage = TypedDict(
 )
 
 
-TalonDocstringHook = Union[
-    Callable[[str, str], Optional[str]],
-    dict[str, dict[str, str]],
+TalonDocstringHook: TypeAlias = Union[
+  _TalonDocstringHook_Callable,
+  _TalonDocstringHook_Dict,
 ]
+
+_TalonDocstringHook_Callable: TypeAlias = Callable[[str, str], Optional[str]]
+_TalonDocstringHook_Dict: TypeAlias = dict[str, dict[str, str]]
 
 
 def setup(app: Sphinx) -> dict[str, Any]:
     app.add_domain(TalonDomain)
-    app.add_config_value(name="talon_docstring_hook", default=None, rebuild="env")
+    
+    app.add_config_value(
+      name="talon_docstring_hook",
+      default=None,
+      rebuild="env",
+      types=[
+        _TalonDocstringHook_Callable,
+        _TalonDocstringHook_Dict,
+      ]
+    )
+    
     return {
         "version": __version__,
         "parallel_read_safe": False,
