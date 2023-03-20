6584954d0ea4981fe738b743823faa328b213510
Wen Kokke <wenkokke@users.noreply.github.com>
2023-03-20 20:00:22 +0000

    Update documentation style

--- example/docs/conf.py
+++ example/docs/conf.py
@@ -9,7 +9,9 @@
 import os
 import sys
 
-project = "example"
+from sphinx.application import Sphinx
+
+project = "TalonDoc"
 copyright = "2022, Wen Kokke"
 author = "Wen Kokke"
 release = "0.1.4"
@@ -19,16 +21,47 @@ release = "0.1.4"
 
 sys.path.append(os.path.abspath("../.."))
 
-extensions = ["talondoc.sphinx"]
+extensions = [
+    # Enables support for Markdown
+    # https://www.sphinx-doc.org/en/master/usage/markdown.html
+    "myst_parser",
+    # Enables support for tabs
+    # https://sphinx-tabs.readthedocs.io/en/latest/#sphinx-tabs
+    "sphinx_tabs.tabs",
+    # Enable support for Talon
+    "talondoc.sphinx",
+]
+
+# -- Options for MyST --------------------------------------------------------
+
+myst_enable_extensions = [
+    # Enables colon fence directives
+    # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#syntax-colon-fence
+    "colon_fence",
+    # Enables definition lists
+    # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#definition-lists
+    "deflist",
+]
+
+# -- Options for Sphinx Tabs -------------------------------------------------
+
+# Disable tab closing.
+# https://sphinx-tabs.readthedocs.io/en/latest/#sphinx-configuration
+sphinx_tabs_disable_tab_closing = True
+
+# Disable default CSS stylesheet.
+# Custom stylesheet is added under 'Options for HTML output'.
+# # https://sphinx-tabs.readthedocs.io/en/latest/#sphinx-configuration
+sphinx_tabs_disable_css_loading = True
 
 
 # -- Options for TalonDoc ----------------------------------------------------
 
 talon_package = {
-  'path': '../knausj_talon',
-  'name': 'user',
-  'exclude': ['conftest.py', 'test/**'],
-  'trigger': 'ready'
+    "path": "../knausj_talon",
+    "name": "user",
+    "exclude": ["conftest.py", "test/**"],
+    "trigger": "ready",
 }
 
 # def talon_docstring_hook(sort: str, name: str) -> Optional[str]:
@@ -38,92 +71,13 @@ talon_package = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-import sphinx_bootstrap_theme
 
-html_theme = "bootstrap"
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
-html_theme_options = {
-    "navbar_title": "TalonDoc",
-    "navbar_sidebarrel": False,
-    "navbar_pagenav": True,
-    "navbar_links": [
-        ("knausj_talon", "./knausj_talon/index"),
-    ],
-    "bootswatch_theme": "Pulse",
-}
 
-# (Optional) Logo. Should be small enough to fit the navbar (ideally 24x24).
-# Path should be relative to the ``_static`` files directory.
-# html_logo = "my_logo.png"
+# import sphinx_bootstrap_theme
 
-# Theme options are theme-specific and customize the look and feel of a
-# theme further.
-html_theme_options = {
-    # Navigation bar title. (Default: ``project`` value)
-    'navbar_title': "TalonDoc",
-
-    # Tab name for entire site. (Default: "Site")
-    'navbar_site_name': "TalonDoc",
-
-    # A list of tuples containing pages or urls to link to.
-    # Valid tuples should be in the following forms:
-    #    (name, page)                 # a link to a page
-    #    (name, "/aa/bb", 1)          # a link to an arbitrary relative url
-    #    (name, "http://example.com", True) # arbitrary absolute url
-    # Note the "1" or "True" value above as the third argument to indicate
-    # an arbitrary url.
-    'navbar_links': [
-        ("knausj_talon", "./knausj_talon/index"),
-    ],
-
-    # Render the next and previous page links in navbar. (Default: true)
-    'navbar_sidebarrel': False,
-
-    # Render the current pages TOC in the navbar. (Default: true)
-    'navbar_pagenav': False,
-
-    # Tab name for the current pages TOC. (Default: "Page")
-    'navbar_pagenav_name': "Page",
-
-    # Global TOC depth for "site" navbar tab. (Default: 1)
-    # Switching to -1 shows all levels.
-    'globaltoc_depth': 2,
-
-    # Include hidden TOCs in Site navbar?
-    #
-    # Note: If this is "false", you cannot have mixed ``:hidden:`` and
-    # non-hidden ``toctree`` directives in the same page, or else the build
-    # will break.
-    #
-    # Values: "true" (default) or "false"
-    'globaltoc_includehidden': "true",
-
-    # HTML navbar class (Default: "navbar") to attach to <div> element.
-    # For black navbar, do "navbar navbar-inverse"
-    'navbar_class': "navbar navbar-inverse",
-
-    # Fix navigation bar to top of page?
-    # Values: "true" (default) or "false"
-    'navbar_fixed_top': "true",
-
-    # Location of link to source.
-    # Options are "nav" (default), "footer" or anything else to exclude.
-    'source_link_position': "nav",
-
-    # Bootswatch (http://bootswatch.com/) theme.
-    #
-    # Options are nothing (default) or the name of a valid theme
-    # such as "cosmo" or "sandstone".
-    #
-    # The set of valid themes depend on the version of Bootstrap
-    # that's used (the next config option).
-    #
-    # Currently, the supported themes are:
-    # - Bootstrap 2: https://bootswatch.com/2
-    # - Bootstrap 3: https://bootswatch.com/3
-    'bootswatch_theme': "readable",
-
-    # Choose Bootstrap version.
-    # Values: "3" (default) or "2" (in quotes)
-    'bootstrap_version': "3",
-}
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+html_css_files = [
+    # Add a custom stylesheet for Sphinx Tabs
+    "css/custom-tabs.css",
+]
