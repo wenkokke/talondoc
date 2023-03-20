d8fc5498a6485f70406932abea681ba89b96bae4
Wen Kokke <wenkokke@users.noreply.github.com>
2023-03-20 01:18:24 +0000

    Add types for talon_packages and talon_docstring_hook

--- talondoc/sphinx/__init__.py
+++ talondoc/sphinx/__init__.py
@@ -1,4 +1,6 @@
-from typing import Any
+from typing import Any, Optional, Sequence, Union
+from typing_extensions import TypedDict
+from pathlib import Path
 
 from sphinx.application import Sphinx
 
@@ -6,9 +8,24 @@ from .. import __version__
 from .domains import TalonDomain
 
 
+TalonPackage = TypedDict('TalonPackage', {
+  'path': Union[str, Path],
+  'name': Optional[str],
+  'include': Union[None, str, Sequence[str]],
+  'exclude': Union[None, str, Sequence[str]],
+  'trigger': Union[None, str, Sequence[str]],
+})
+
+
+TalonDocstringHook = Union[
+  Callable[[str, str], Optional[str]],
+  dict[str, dict[str, str]],
+]
+
+
 def setup(app: Sphinx) -> dict[str, Any]:
     app.add_domain(TalonDomain)
-    app.add_config_value(name="talondoc_docstring_hook", default=None, rebuild="env")
+    app.add_config_value(name="talon_docstring_hook", default=None, rebuild="env")
     return {
         "version": __version__,
         "parallel_read_safe": False,
