fc440292cc6cda03fd36b62d0e1438e21d839f82
Wen Kokke <wenkokke@users.noreply.github.com>
2023-03-20 00:51:16 +0000

    Update domains.py

--- talondoc/sphinx/domains.py
+++ talondoc/sphinx/domains.py
@@ -2,10 +2,10 @@ from sphinx.domains import Domain
 from sphinx.environment import BuildEnvironment
 
 from ..registry import Registry
-from ..sphinx.directives.command.hlist import TalonCommandHListDirective
-from ..sphinx.directives.command.table import TalonCommandTableDirective
 from ..util.logging import getLogger
 from .directives.command import TalonCommandDirective
+from .directives.command.hlist import TalonCommandHListDirective
+from .directives.command.table import TalonCommandTableDirective
 from .directives.package import TalonPackageDirective
 from .directives.user import TalonUserDirective
 
