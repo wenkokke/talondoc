c874fbff5257530db81b79a85afdfed5bd7f30bc
Wen Kokke <wenkokke@users.noreply.github.com>
2023-05-07 20:12:06 +0100

    Bump version 0.3.0 -> 0.3.1

--- example/docs/conf.py
+++ example/docs/conf.py
@@ -14,7 +14,7 @@ from sphinx.application import Sphinx
 project = "TalonDoc"
 copyright = "2022, Wen Kokke"
 author = "Wen Kokke"
-release = "0.3.0"
+release = "0.3.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
