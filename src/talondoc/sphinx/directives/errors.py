97ce242e038557cab60843673205d2c4a912f939
Wen Kokke <wenkokke@users.noreply.github.com>
2023-05-21 19:14:49 +0100

    Undo resolve in ambiguous signatures

--- src/talondoc/sphinx/directives/errors.py
+++ src/talondoc/sphinx/directives/errors.py
@@ -14,14 +14,10 @@ class AmbiguousSignature(Exception):
     signature: str
     candidates: Sequence[str] = field(default_factory=tuple)
 
-    def _resolved_location(self) -> str:
-        path, rest = self.location.split(":", maxsplit=2)
-        return f"{Path(path).resolve(strict=False)}:{rest}"
-
     def __str__(self) -> str:
         return "\n".join(
             [
-                f"{self._resolved_location()}:"
+                f"{self.location}:"
                 f"Multiple matches found for signature '{self.signature}'",
                 *map(lambda dsc: f"- {dsc}", self.candidates),
             ]
