90d1e94389e499cf7cfa78d1471861e7c7c24a45
Wen Kokke <wenkokke@users.noreply.github.com>
2023-05-23 16:13:31 +0100

    Build without venv

--- scripts/pyinstaller.sh
+++ scripts/pyinstaller.sh
@@ -3,12 +3,6 @@
 # See: https://stackoverflow.com/a/4774063
 ROOT_DIR="$(dirname "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )" )"
 
-# Create a virtual environment:
-python -m venv "${ROOT_DIR}/_venv"
-
-# Activate the virtual environment:
-source "${ROOT_DIR}/_venv/bin/activate"
-
 # Install TalonDoc
 pip install -q "${ROOT_DIR}"
 
@@ -20,9 +14,14 @@ pip install -q myst_parser
 pip install -q sphinx_rtd_theme
 pip install -q sphinx_tabs
 
+# Create main script:
+mkdir -p "${ROOT_DIR}/bin"
+echo 'import talondoc'     >  "${ROOT_DIR}/bin/__main__.py"
+echo 'talondoc.talondoc()' >> "${ROOT_DIR}/bin/__main__.py"
 
 # Compile TalonDoc with PyInstaller
 pyinstaller                                                                                                    \
+  --name "talondoc"                                                                                            \
   --distpath "bin"                                                                                             \
   --python-option "-X utf8"                                                                                    \
   --collect-data "tree_sitter_talon"                                                                           \
@@ -33,4 +32,4 @@ pyinstaller                                                                     
   --collect-data "sphinx_rtd_theme" --collect-submodules "sphinx_rtd_theme" --hidden-import "sphinx_rtd_theme" \
   --collect-data "sphinx_tabs"      --collect-submodules "sphinx_tabs"      --hidden-import "sphinx_tabs"      \
   --onefile                                                                                                    \
-  "${ROOT_DIR}/_venv/bin/talondoc"
+  "${ROOT_DIR}/bin/__main__.py"
