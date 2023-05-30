9247fb07c6ae56506b0cc91506c0b2a8f24ea665
Wen Kokke <wenkokke@users.noreply.github.com>
2023-05-30 12:21:24 +0100

    Remove width attribute from fragtable

--- src/talondoc/sphinx/_util/addnodes/fragtable.py
+++ src/talondoc/sphinx/_util/addnodes/fragtable.py
@@ -20,12 +20,6 @@ class fragtable(nodes.table):  # type: ignore[misc]
 
 def visit_fragtable_html(self: HTML5Translator, node: fragtable) -> None:
     atts: dict[str, Any] = {"classes": ["fragtable"]}
-    if "width" in node:
-        width = cast(Optional[int], node["width"])
-        if width:
-            atts["style"] = (
-                f"column-width: {width}px;" f"column-fill: balance;" f"width: 100%;"
-            )
     tag = self.starttag(node, "div", **atts)
     self.body.append(tag)
     self.visit_table(node)
