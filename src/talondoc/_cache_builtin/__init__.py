4d49c74dc175bd477f20468f890869c8a1d42ab3
Thomas Atkins <dev.csnova@gmail.com>
2025-02-10 06:51:50 +0000

    feat(Ruff): :recycle: Added flake8-use-pathlib (PTH)
    Added ruff rule to use the safer Pathlib libary instead of the os

--- src/talondoc/_cache_builtin/__init__.py
+++ src/talondoc/_cache_builtin/__init__.py
@@ -1,6 +1,6 @@
 import json
-import os
 from collections.abc import Mapping
+from pathlib import Path
 
 from .._util.logging import getLogger
 from ..analysis.live import TalonRepl
@@ -18,7 +18,7 @@ def cache_builtin(output_dir: str) -> None:
             for cls, objs in builtin.items():
                 if isinstance(objs, Mapping):
                     _LOGGER.info(f"Found {len(objs)} {cls.lower()}s")
-        output_path = os.path.join(output_dir, "talon.json")
+        output_path = Path(output_dir) / "talon.json"
         with open(output_path, "w") as f:
             json.dump(builtin, f)
         _LOGGER.info(f"Wrote {output_path}")
